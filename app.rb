69e5b800ac267887d073b280cf545dbb1b03d86d
h-morimoto-gs <h.morimoto@genbasupport.com>
2026-09-18 13:26:29 +0900

    メモの保存先をJSONからPostgreSQLに変更

--- app.rb
+++ app.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'fileutils'
-require 'json'
 require 'pg'
-require 'securerandom'
-
-MEMOS_FILE = 'data/memos.json'
 
 configure do
   connection = PG.connect(dbname: 'memo_app')
@@ -19,19 +14,8 @@ helpers do
     settings.db
   end
 
-  def load_memos
-    return [] unless File.exist?(MEMOS_FILE)
-
-    JSON.parse(File.read(MEMOS_FILE), symbolize_names: true)
-  end
-
-  def save_memos(memos)
-    FileUtils.mkdir_p(File.dirname(MEMOS_FILE))
-    File.write(MEMOS_FILE, JSON.pretty_generate(memos))
-  end
-
-  def find_memo(id, memos = load_memos)
-    memos.find { |memo| memo[:id] == id }
+  def find_memo(id)
+    db.exec_params('SELECT * FROM memos WHERE id = $1', [id]).first
   end
 
   def display_title(title)
@@ -61,40 +45,30 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  memos = load_memos
-  memos << {
-    id: SecureRandom.uuid,
-    title: params[:title],
-    content: params[:content]
-  }
-  save_memos(memos)
+  db.exec_params('INSERT INTO memos (title, content) VALUES ($1, $2)', [params[:title], params[:content]])
   redirect '/memos'
 end
 
 get '/memos/:id' do
-  @memo = find_memo(params[:id])
+  @memo = find_memo(params[:id].to_i)
   halt 404 unless @memo
   erb :show
 end
 
 get '/memos/:id/edit' do
-  @memo = find_memo(params[:id])
+  @memo = find_memo(params[:id].to_i)
   halt 404 unless @memo
   erb :edit
 end
 
 patch '/memos/:id' do
-  memos = load_memos
-  memo = find_memo(params[:id], memos)
-  halt 404 unless memo
-  memo[:title] = params[:title]
-  memo[:content] = params[:content]
-  save_memos(memos)
+  result = db.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3',
+                          [params[:title], params[:content], params[:id].to_i])
+  halt 404 if result.cmd_tuples.zero?
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = load_memos.reject { |memo| memo[:id] == params[:id] }
-  save_memos(memos)
+  db.exec_params('DELETE FROM memos WHERE id = $1', [params[:id].to_i])
   redirect '/memos'
 end
